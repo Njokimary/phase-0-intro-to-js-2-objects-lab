39d880186d391b6d90da7e461e53a7f7a6afbdc7
MARY NJOKI <marynjoki738@gmail.com>
2023-07-14 14:46:00 +0300

    worked on the objects lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+// Initialize employee object
+let employee = {
+  name: 'Mary Njoki',
+  streetAddress: '123 bul Street',
+};
+
+// Function to update employee object non-destructively
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+
+// Function to update employee object destructively
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Function to delete a property from employee object non-destructively
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Function to delete a property from employee object destructively
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
